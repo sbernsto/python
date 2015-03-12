4a9bad5fef9c110f04ffa9d9eb42144d7815ea78
Stephan Bernstorf <Stephan.Bernstorf@jhuapl.edu>
2015-03-12 08:31:33 -0400

    Day 2 Program, simple tip calculator.

---
+++ TipCalculator/main.py
@@ -0,0 +1,12 @@
+# main.py
+# This program calculate the tip each person should give.
+
+
+bill = 50
+people = 2
+percentTip = 0.18
+
+tip = bill/people*percentTip
+
+print ("Each person should tip $",tip,"dollars")
+
