52a85fefbf6ebfd2b1dc148035bf444e26b1d578
Stephan Bernstorf <Stephan.Bernstorf@jhuapl.edu>
2015-03-15 18:32:28 -0400

    Final Version of Magic 8 Ball

--- Magic8Ball/Magic8Ball.py
+++ Magic8Ball/Magic8Ball.py
@@ -2,3 +2,21 @@
 # This program opens with a welcome screan, asks the user for an input
 # and then provides an answer from a list of 20 possibilities.
 
+import time
+import random
+
+print("Welcome to the Magic 8-Ball!\n")
+
+# Create list of random answers
+list8Ball = ['Of Course', 'Definitely', 'The future is unclear','Yes', 'No', 'Maybe']
+
+# Set while loop variable to True
+answer = True
+
+#While loop
+while answer == True:
+    print("Please think of your question: ")
+    time.sleep(5)
+    print("My answer is ", random.choice(list8Ball))
+    
+    answer = input("Do you want to ask another? (True or False)")
