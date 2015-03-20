a481f21d8ee1cb8a250d878b75b5a00f506b1509
Stephan Bernstorf <Stephan.Bernstorf@jhuapl.edu>
2015-03-19 22:56:07 -0400

    Initial concept

--- CoinEstimator/coinEstimator.py
+++ CoinEstimator/coinEstimator.py
@@ -4,51 +4,32 @@
 #           and output how many coins they have, how many wrappers they need, and how 
 #           much all the coins are worth.
 
-#Weights of each coin (grams)
-pennyWeight = 2.5
-nickleWeight = 5
-dimeWeight = 2.268
-quarterWeight = 5.67
+import math
+print("Welcome To The Coin Estimator, v2.0")
 
-#Number of coins in each wrapper
-pennyWrapper = 50
-nickleWrapper = 40
-dimeWrapper = 50
-QuarterWrapper = 40
-
-#Welcome Screen
-print("Welcome To The Coin Estimator")
-
-# User input
-pennies = input("What is the weight of your pennies?:")
-nickles = input("What is the weight of your nickles?")
-dimes = input("What is the weight of your dimes?: ")
-quarters = input("What is the weight of your quarters?:")
-
-#Convert input strings to floating point numbers
-pennies = float(pennies)
-nickles = float(nickles)
-dimes = float(dimes)
-quarters = float(quarters)
-
-#Calculate how many coins you have
-numPennies = pennies/pennyWeight
-numNickles = nickles/nickleWeight
-numDimes = dimes/dimeWeight
-numQuarters = quarters/quarterWeight
-
-# Print out how many of each coin you have
-print("You have the following coins:")
-print("Pennies:",int(numPennies))
-print("Nickles: ",int(numNickles))
-print("Dimes: ",int(numDimes))
-print("Quarters: ", int(numQuarters))
-
-#Determine and print the number of wrappers needed
-
-#Print out the total value of all the coins
-totalValue = numPennies*0.01 + numNickles*0.05 + numDimes*0.1 + numQuarters*0.25
-print("The total value of your coins is: $", round(totalValue,2))
+coin_types = [
+["pennies", 2.5, 0.01, 50],
+["nickles", 5.0, 0.05, 40],
+["dimes", 2.268, 0.1, 50],
+["quarters", 5.67, 0.25, 40]
+]
 
 
+bank = 0.0
 
+for coin, weight, value, roll in coin_types:
+    weight_choice = input("Are your",coin,"weighted in g or lbs?")
+    while weight_choice.lower() != "g" and weight_choice.lower() != "lbs":
+        weight_choice = input("Please enter g or lbs:")
+        
+    coinWeight = float(input("Enter the weight of your",coin,":"))
+    if weight_choice.lower == "lbs":
+        coinWeight *= 453.592
+        
+    coinCount = math.ceil(coinWeight/weight)
+    coinWrapper = math.ceil(coinCount/roll)
+    print("You have approx. %s %s and you will need %s rolls." %str(coinCount), coin, str(coinWrapper))
+    
+    bank += (coinCount * value)
+    
+print("You have approx. $",bank, sep='')
---
+++ coinExchange/coinExchange.py
@@ -0,0 +1,5 @@
+#coinExchange.PY
+# Date: 19 March 2015
+# Author: S. A. Bernstorf
+# Purpose: Create a program that simulates a cashier calculating change in coins
+
